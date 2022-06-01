3d81873697ef443cb7f89d0d983658f26b428a33
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-06-01 17:53:51 +0900

    add simple home and about page and router setting

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <router-view></router-view>
+
+    <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
