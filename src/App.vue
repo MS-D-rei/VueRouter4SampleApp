06fc1beae464b66a5317c37852c676fe55365f1f
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-06-05 23:18:37 +0900

    add nav component to put nav together

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
-<script setup></script>
+<script setup>
+import TheNavigationVue from './components/TheNavigation.vue';
+</script>
 
 <template>
   <header>
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/brazil">Brazil</router-link>
-      <router-link to="/hawaii">Hawaii</router-link>
-      <router-link to="/jamaica">Jamaica</router-link>
-      <router-link to="/panama">Panama</router-link>
-    </div>
+    <TheNavigationVue />
     <!-- anchor tag also can change to the about page but refresh the page. -->
     <!-- <a href="/about">a tag for About</a> -->
     <div class="container">
