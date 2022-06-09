b5e8efdc7e17169e662a605403038eebef7284ea
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-06-09 19:44:50 +0900

    add AppLink to distinguish internal external link

---
+++ src/components/AppLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a v-if="isExternal" :href="props.to">
+    <slot />
+  </a>
+  <router-link v-else v-bind="props">
+    <slot />
+  </router-link>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  // this enables to use 'to' props.
+  ...RouterLink.props,
+});
+
+const isExternal = computed(() => {
+  return typeof props.to === "string" && props.to.startsWith("https");
+});
+</script>
+
+<style lang="scss" scoped></style>
